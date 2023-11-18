eaefa4bb7e51138bc77f80159b1f595e6725302d
Benjamin Binder <bind.ben@gmail.com>
2023-11-18 11:53:34 +0100

    Publish content

---
+++ README.md
@@ -0,0 +1,150 @@
+<div align="center">
+
+# <img src="docs/raspberry-pi.svg" style="height: 1em; aspect-ratio: 1/1; " aria-hidden="true"> Himbeer Verfolgung
+
+A simple RFID demo using a Raspberry Pi and Python
+
+</div>
+
+
+## 💡 About The Project
+
+This is a very simplistic project to demonstrate the use of RFID technology.
+It uses the [NXP MFRC-522 chip](https://www.nxp.com/docs/en/data-sheet/MFRC522.pdf) to communicate with [Mifare Classic EV1 1K](https://www.nxp.com/docs/en/data-sheet/MF1S50YYX_V1.pdf) RFID tags. 
+
+It is intended to be used in the lecture "Soziotechnische Studien: Container, Paletten, Verpackungen" taught by Prof. Dr. Martin Binder at [PH-Weingarten](https://www.ph-weingarten.de).
+
+The project can be used in conjunction with this [RESTful](https://github.com/ITBenni/himbeer-verfolgung-backend) backend and this [web frontend](https://github.com/ITBenni/himbeer-verfolgung-frontend). 
+
+
+## Getting Started
+
+This project consists of two components: a [hardware component](#raspberry-pi-setup) and a [software component](#software-setup).
+To get this project up and running follow these steps.
+
+### 🛒 Prerequisites
+
+This project is intended to run on a Raspberry Pi.
+You will need the following hardware: 
+
+- [Raspberry Pi 3+/4/5](https://www.berrybase.de/en/raspberry-pi-4-computer-modell-b-4gb-ram)
+- Micro SD Card (16GB)
+- [RC522 RFID Modul](https://www.reichelt.de/entwicklerboards-rfid-modul-nxp-mfrc-522-debo-rfid-rc522-p192147.html)
+- Breadboard Wire
+- Breadboard
+
+
+### 🥧 Raspberry Pi setup
+
+#### Installation
+
+Follow [this tutorial](https://www.raspberrypi.com/documentation/computers/getting-started.html) to setup your Pi up as a *headless* computer.
+
+In the [Imager software](https://www.raspberrypi.com/software/) select *Raspberry Pi OS Lite (64-bit)* as operating system.
+Edit the OS customization settings, so they look something like this:
+
+![](docs/raspberry-setup-1.png)
+![](docs/raspberry-setup-2.png)
+
+Replace the *wireless LAN SSID* and *password* with the ones from your wireless network.
+
+To access your Pi, either connect it to a monitor and keyboard, or use SSH to access it. 
+In the later case, use these tutorials to connect to your Pi: [for Windows](https://www.raspberrypi.com/documentation/computers/remote-access.html#secure-shell-from-windows-10)/[for Mac](https://www.raspberrypi.com/documentation/computers/remote-access.html#secure-shell-from-linux-or-mac-os). 
+To log in, use the username and password you configured in the Imager software. 
+
+The ssh command should look like this:
+
+```sh
+ssh pi@raspberrypi-1.local
+```
+
+At the end of this step, you should have a command line in front of you where you can enter commands. 
+You can check it by running the command `whoami` which shoud give you `pi` ;)
+
+
+#### Configuration
+
+To use the GPIO-pins of the Pi, use the [raspi-config tool](https://www.raspberrypi.com/documentation/computers/configuration.html#raspi-config) to [enable the SPI interface](https://www.raspberrypi.com/documentation/computers/configuration.html#spi) under [System Options](https://www.raspberrypi.com/documentation/computers/configuration.html#system-options). 
+
+Shutdown your Pi to set up up the wiring using the following command:
+
+```sh
+sudo shutdown now
+```
+
+
+### 🪛 Hardware setup
+
+Connect the RFID reader to your Pi. 
+You can follow the [instructions in the original post](https://pimylifeup.com/raspberry-pi-rfid-rc522/#wiring-the-rfid-rc522).
+
+It should look like this: 
+
+![Wiring of the Raspberry Pi and RC522 RFID reader](https://pimylifeup.com/wp-content/uploads/2017/10/RFID-Fritz-v2.png)
+
+This [Raspberry Pi pinout guide](https://pinout.xyz) might also be helpful. 
+
+### 🧑‍💻 Software Setup
+
+The following steps should all be completed on your Pi.
+
+#### Prerequisites
+
+This demo uses the [python programming language](https://docs.python.org/3/tutorial/index.html). 
+It comes preinstalled on Raspbian OS, but running the demo requires a few additional tools. 
+Install them using the following command: 
+
+```sh
+sudo apt install python3-dev python3-pip git
+```
+
+The two important tools used here are [pip](https://pip.pypa.io/en/stable/) and [git](https://git-scm.com/video/what-is-version-control). 
+
+#### Installation
+
+Download the sourcecode from this repository by running the following command:
+
+```sh
+git clone https://github.com/ITBenni/himbeer-verfolgung-rfid.git && cd himbeer-verfolgung-rfid
+```
+
+Install additional project dependencies by running:
+
+```sh
+pip install -r requirements.txt
+```
+
+This mainly install the python library necessary for interacting with the RF522 RFID module. 
+
+
+## 🪱 Usage
+
+The main program resides in [rfid.py](/rfid.py).
+Run it using this command:
+
+```sh
+python3 rfid.py
+```
+
+
+## 🤝 Contributing
+
+Any contributions are welcome :)
+If you find any errors or if you have feedback, please open an [issue on Github](https://github.com/ITBenni/himbeer-verfolgung-rfid/issues).
+
+
+
+## 📜 License
+
+Distributed under the MIT License. See [LICENSE](LICENSE) for more information.
+
+
+## 🙏 Acknowledgments
+
+This project stands on the shoulders of giants. Those are: 
+
+- [Ondřej Ondryá'š (ondryaso) RC522 python library](https://github.com/ondryaso/pi-rc522)
+- [Angus Young's (gus-0) original post on PiMyLifeUp](https://pimylifeup.com/raspberry-pi-rfid-rc522/#wiring-the-rfid-rc522)
+- [Mario Gómez's (mxgxw) RC522 python library](https://github.com/mxgxw/MFRC522-python)
+
+Without these amazing people this project would't have been possible. 
---
+++ rfid.py
@@ -0,0 +1,172 @@
+from pirc522 import RFID
+import time
+import signal
+import sys
+import struct
+from datetime import datetime
+import logging
+
+import uuid
+import requests
+import socket
+
+API_ENPOINT='https://37dff9c1-b164-4821-93d5-59684591f4f0.ma.bw-cloud-instance.org'
+
+
+reader = RFID()
+util = reader.util()
+
+
+# Set up the logging output printed to the console
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Enable additional output when program is in debug mode
+if logger.getEffectiveLevel() == logging.DEBUG:
+	util.debug = True
+
+
+# Get a unique device ID based on the MAC address of a network interface
+# This ID never changes between device reboots or even reinstalls
+# UUID based on RFC4122 were unsuitable since they may change between reboots
+id = uuid.UUID(int=uuid.getnode())
+station_ID = str(id)
+
+
+# Register this reader with the server
+r = requests.post(f'{API_ENPOINT}/api/stations', json={
+	'station_uuid': station_ID,
+	'name': socket.gethostname()
+})	
+
+
+# The program should be launchable from the CLI
+# Therefore it is criticial to gracefully shut down when SIGKILL event is sent (e.g. via CTRL+C)
+# Catching a KeyboardInterrupt allows us to handle the event properly
+try:
+	logging.info('Starting poll loop')
+
+	while True:
+		# Wait for a tag to come into sending range
+		logging.info('Waiting for tag\n')
+		reader.wait_for_tag()
+
+		# Request tag
+		(error, data) = reader.request()
+		if error:
+			continue
+
+		logging.info('Tag(s) detected')
+
+		# Make sure we are only communicating with one tag
+		(error, uid) = reader.anticoll()
+		if error:
+			continue
+		
+		# Print UID
+		tag_ID = f'{uid[0]}-{uid[1]}-{uid[2]}-{uid[3]}'
+		logging.info(tag_ID)
+
+		# Send update to server
+		r = requests.post(f'{API_ENPOINT}/api/stations/{station_ID}/tags', json={
+			'tag_uuid': tag_ID,
+			'last_seen': 0,
+			'previously_seen': 0
+		})	
+
+		logging.info(r.status_code)
+
+		# Set tag as used in util. This will call RFID.select_tag(uid)
+		util.set_tag(uid)
+
+		# Save authorization info (key B) to util
+		# This is needed later when performing operations on the tag
+		util.auth(reader.auth_b, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
+		
+
+		# Show the last seen information
+		# The timestamp is stored in the first block of the first usable sector (2)
+		timestamp_address = util.block_addr(1, 0)
+		logging.debug(f'Timestamp block address: {timestamp_address}')
+
+		# Perform an initial authentication for the sector  
+		util.do_auth(timestamp_address)
+		
+		# Read the timestamp 
+		error, data = reader.read(timestamp_address)
+		if error: 
+			logging.error('Last seen timestamp could not been read')
+		else:
+			# pirc522 returns data read as a 16-byte aligned list of bytes  (y???????!?)
+			# To make the data usable, we need to convert them into a regular integer
+			# Bytes are interpreted from little endian
+			data = int.from_bytes(bytes(data), "little")
+
+			# A unix timestamp isn't easy to read
+			# To make our live easier we convert it into a string representation using a regular format
+			# See https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes for more information
+			timestamp_human_readable = datetime.utcfromtimestamp(data).strftime('%Y-%m-%d %H:%M:%S')
+			logging.info(f'Tag last seen on {timestamp_human_readable}')
+
+			# Update the last seen timestamp with the current time
+			# Get a 4 byte unix timestamp
+			now = int(time.time())
+			logging.debug(datetime.utcfromtimestamp(now).strftime('%Y-%m-%d %H:%M:%S'))
+
+			# pirc522 expects data to be written to also be in a 16-byte aligned list of bytes
+			# To do so we have to convert the regular int timestamp into a byte object and then pad it accordingly
+			# First convert the int into a byte object
+			# Data stored on tags are stored in little endian (https://www.nxp.com/docs/en/data-sheet/MF1S50YYX_V1.pdf, p. 9)
+			data = struct.pack("<i", now)
+
+			# Then pad the data to 16 bytes by adding 12 bytes of zeros
+			data = data + bytearray(12)
+
+			# Write the data to the tag
+			error = reader.write(timestamp_address, data)
+			if error:
+				logging.error('Write of timestamp not successful')
+
+
+		# Read the previously seen counter and show it 
+		# The counter is stored in the first block of the second usable sector (3)
+		# Get address of sector 3 Block 1
+		counter_address = util.block_addr(2, 0)
+		logging.debug(f'Counter block address: {counter_address}')
+
+		# Because we are using a new sector we have to authenticate again
+		util.do_auth(counter_address)
+
+		# Read the counter
+		error, data = reader.read(counter_address)
+		if error: 
+			logging.error('Previously seen counter could not been read')
+		else:
+			# The counter is store in the first byte of the first block
+			counter = data[0]
+
+			if counter <= 0:
+				logging.info('I\'ve never met this tag in my life')
+			else:
+				logging.info(f'Tag previously seen {counter} times. ')
+
+			# Increment the counter and save on tag
+			# Careful: Because the variable is actually only one byte, it will overflow after 255 writes
+			# This could be fixed by incrementing the variables width
+			data[0] = counter + 1
+
+			error = reader.write(counter_address, data)
+			if error:
+				logging.error('Write of previously seen counter not successful')
+
+
+		# After finishing all operations on the tag we can deauthenticate and remove all credentials from memory
+		util.deauth()
+
+		# Wait for 3 seconds before doing anything else
+		# This isn't technically necessary, it's just to make the demonstration a little easier
+		time.sleep(3)
+
+
+except KeyboardInterrupt:
+	reader.cleanup()		
